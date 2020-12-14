acb927ab1454517f0c3fdf8049cc9880c4bf4bf0
Lisa Bartfeld <LisaBartfeld@gmail.com>
2020-12-13 22:28:03 -0600

    working on search feature, incomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   title = 'orbit-report';
 
   sourceList: Satellite[];
-
+  displayList: Satellite[];
   // constructor() {
   //   this.sourceList = [
   //     new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
@@ -23,6 +23,7 @@ export class AppComponent {
   // }
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -40,9 +41,32 @@ export class AppComponent {
           this.sourceList.push(satellite);
         }
  
+
+// make a copy of the sourceList to be shown to the user
+this.displayList = this.sourceList.slice(0);
+
+
        }.bind(this));
     }.bind(this));
+    
  
  }
+ //search methodabcd1234
+ 
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
+
+
  
 }
